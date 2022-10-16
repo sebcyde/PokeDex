2db8684e2115568e887715fe0d2cc28e8af61edf
Seb <sebcyde@hotmail.com>
2022-10-16 12:59:07 +0100

    dashboard styling created object refs to create ClosePage function

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,75 +1,103 @@
 import 'aos/dist/aos.css';
 import styled from 'styled-components';
-import React, { useEffect } from 'react';
+import React, { MutableRefObject, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import DivBackground from '../Assets/Sci-Fi/Blue/Hud1.png';
 
 function Dashboard() {
 	const AOS = require('aos');
 	const navigate = useNavigate();
+	const PokeDexRef = useRef<HTMLDivElement>(null);
+	const FavouritesRef = useRef<HTMLDivElement>(null);
+	const MovesRef = useRef<HTMLDivElement>(null);
+	const ItemsRef = useRef<HTMLDivElement>(null);
+	const MachinesRef = useRef<HTMLDivElement>(null);
+	const SettingsRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
 		AOS.init();
 		AOS.refresh();
 	}, []);
 
-	const ClosePage = () => {};
+	const ClosePage = (MenuItem: MutableRefObject<HTMLDivElement | null>) => {};
 
-	const Navigate = async (location: string) => {
-		await ClosePage();
+	const Navigate = async (
+		location: string,
+		MenuItem: MutableRefObject<HTMLDivElement | null>
+	) => {
+		await ClosePage(MenuItem);
 		navigate(location);
 	};
 
-	const MenuItem = styled.div``;
+	const MenuItemStyle = {
+		backgroundImage: `url(${DivBackground})`,
+		backgroundRepeat: 'no-repeat',
+		backgroundSize: '100% 150px',
+		backgroundPosition: '-10%',
+		backgroundColor: 'crimson',
+	};
 
 	return (
 		<div className="w-100 h-100 flex flex-col align-center just-center margin-auto">
 			<div
-				className="black-hex-bg w-70 h-20p col-white bor-white-thin radius-25p pad-thin margin-xthin text-center flex align-center just-center"
+				className="w-70 col-aqua pad-thin margin-xthin text-center flex align-center just-center "
 				data-aos="fade-right"
 				data-aos-duration="700"
-				onClick={() => Navigate('/pokedex')}
+				onClick={() => Navigate('/pokedex', PokeDexRef)}
+				style={MenuItemStyle}
+				ref={PokeDexRef}
 			>
-				<h2 className="margin-0 font-21">PokeDex</h2>
+				<h2 className="margin-0 font-25">PokeDex</h2>
 			</div>
 			<div
-				className="black-hex-bg w-70 h-20p col-white bor-white-thin radius-25p pad-thin margin-xthin text-center flex align-center just-center"
+				className="w-70 col-aqua pad-thin margin-xthin text-center flex align-center just-center"
 				data-aos="fade-left"
 				data-aos-duration="700"
-				onClick={() => Navigate('/favourites')}
+				onClick={() => Navigate('/favourites', FavouritesRef)}
+				style={MenuItemStyle}
+				ref={FavouritesRef}
 			>
-				<h2 className="margin-0 font-21">Favourites</h2>
+				<h2 className="margin-0 font-25">Favourites</h2>
 			</div>
 			<div
-				className="black-hex-bg w-70 h-20p col-white bor-white-thin radius-25p pad-thin margin-xthin text-center flex align-center just-center"
+				className="w-70 col-aqua pad-thin margin-xthin text-center flex align-center just-center"
 				data-aos="fade-right"
 				data-aos-duration="700"
-				onClick={() => Navigate('/moves')}
+				onClick={() => Navigate('/moves', MovesRef)}
+				style={MenuItemStyle}
+				ref={MovesRef}
 			>
-				<h2 className="margin-0 font-21">Moves</h2>
+				<h2 className="margin-0 font-25">Moves</h2>
 			</div>
 			<div
-				className="black-hex-bg w-70 h-20p col-white bor-white-thin radius-25p pad-thin margin-xthin text-center flex align-center just-center"
+				className="w-70 col-aqua pad-thin margin-xthin text-center flex align-center just-center"
 				data-aos="fade-left"
 				data-aos-duration="700"
-				onClick={() => Navigate('/items')}
+				onClick={() => Navigate('/items', ItemsRef)}
+				style={MenuItemStyle}
+				ref={ItemsRef}
 			>
-				<h2 className="margin-0 font-21">Items</h2>
+				<h2 className="margin-0 font-25">Items</h2>
 			</div>
 			<div
-				className="black-hex-bg w-70 h-20p col-white bor-white-thin radius-25p pad-thin margin-xthin text-center flex align-center just-center"
+				className="w-70 col-aqua pad-thin margin-xthin text-center flex align-center just-center"
 				data-aos="fade-right"
 				data-aos-duration="700"
-				onClick={() => Navigate('/machines')}
+				onClick={() => Navigate('/machines', MachinesRef)}
+				style={MenuItemStyle}
+				ref={MachinesRef}
 			>
-				<h2 className="margin-0 font-21">Machines</h2>
+				<h2 className="margin-0 font-25">Machines</h2>
 			</div>
 			<div
-				className="black-hex-bg w-70 h-20p col-white bor-white-thin radius-25p pad-thin margin-xthin text-center flex align-center just-center"
+				className="w-70 col-aqua pad-thin margin-xthin text-center flex align-center just-center"
 				data-aos="fade-left"
 				data-aos-duration="700"
-				onClick={() => Navigate('/settings')}
+				onClick={() => Navigate('/settings', SettingsRef)}
+				style={MenuItemStyle}
+				ref={SettingsRef}
 			>
-				<h2 className="margin-0 font-21">Settings</h2>
+				<h2 className="margin-0 font-25">Settings</h2>
 			</div>
 		</div>
 	);
--- src/Partials/Footer.tsx
+++ src/Partials/Footer.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Footer() {
 	return (
-		<div className="position-absolute w-100 flex just-center bot-0 margin-bottom-thin col-white">
+		<div className="position-absolute w-100 flex just-center bot-0 margin-bottom-thin">
 			<span className="flex flex-col just-center align-center">
-				<a className="margin-0 col-white">View Docs</a>
-				<p className="margin-0">Created by Sebastian</p>
+				<a className="margin-0 col-aqua">View Docs</a>
+				<p className="margin-0 col-aqua">Created by Sebastian</p>
 			</span>
 		</div>
 	);
