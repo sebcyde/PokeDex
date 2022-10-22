f49453e2fcd37448bae023fc2113018213c288de
Seb <sebcyde@hotmail.com>
2022-10-22 21:21:36 +0100

    progress on Items API but sort function for sort isnt working

--- src/Context/SortContext.tsx
+++ src/Context/SortContext.tsx
@@ -6,16 +6,3 @@ type ContextType = {
 export const SortContext = createContext<ContextType>({
 	SortType: '',
 });
-
-// export const SortContext = createContext<ContextType>(['', () => { }]);
-// export const useSort = () => useContext(SortContext);
-
-// export const SortProvider = ({ children }: any) => {
-// 	const [Sort, setSort] = useState('Initial Value');
-
-// 	return (
-// 		<SortContext.Provider value={[Sort, setSort]}>
-// 			{children}
-// 		</SortContext.Provider>
-// 	);
-// };
